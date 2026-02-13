b8cbb7e8c2e56a4f698ca7cba51f3478acefbfc0
Pravya Poojari <poojaripravya60@gmail.com>
2026-02-13 14:32:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,22 @@ import streamlit as st
 import pickle
 import numpy as np
 
-st.title("Heart Disease Prediction")
-
 # Load saved model
 model = pickle.load(open("heart_model.pkl", "rb"))
+
+st.title("❤️ Heart Disease Prediction App")
 
 age = st.number_input("Age")
 sex = st.selectbox("Sex (0 = Female, 1 = Male)", [0,1])
 chol = st.number_input("Cholesterol")
 
 if st.button("Predict"):
-    data = np.array([[age, sex, chol]])
-    result = model.predict(data)
+    input_data = np.array([[age, sex, chol]])
+    prediction = model.predict(input_data)
 
-    if result[0] == 1:
+    if prediction[0] == 1:
         st.error("High Risk of Heart Disease")
     else:
         st.success("Low Risk of Heart Disease")
+
 
